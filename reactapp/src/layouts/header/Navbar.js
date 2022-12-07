364c478a18451013854e05e0e22ee1baa2325894
Bishal Shrestha <bbrobishal@gmail.com>
2022-12-07 14:57:00 +0545

    Making Routing and setup project folder structure

---
+++ reactapp/src/layouts/header/Navbar.js
@@ -0,0 +1,17 @@
+
+const Navbar = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+];
+
+export default Navbar;
