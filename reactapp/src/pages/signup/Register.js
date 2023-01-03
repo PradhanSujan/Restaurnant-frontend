c26366aba03fdc6021852a69053aa6236a5f8b04
Sujan <pradhansujan730@gmail.com>
2023-01-03 11:59:52 +0545

    Add SignUp component

--- reactapp/src/pages/signup/Register.js
+++ reactapp/src/pages/signup/Register.js
@@ -1,95 +1,66 @@
 import React from "react";
-import "./styles.css";
+import FormInput from "./Form-input.component";
 
 const Register = () => {
   return (
     <>
+    <div style={{height:"700px", width:"100%",paddingTop:"32px"}}>
+      <div
+        style={{
+          width: "480PX",
+          // minHeight: "400px",
+          display: "flex",
+          justifyContent: "space-evenly",
+          margin: "0 auto",
+          alignItems: "center",
+          flexDirection: "column",
+          boxShadow: "0px 0 31px 0px rgb(0 0 0 / 10%",
+          backgroundColor: " rgba(255, 255, 255, 0.4)",
+          borderRadius: "20px",
+        }}
+      >
+        <h1>Sign Up</h1>
+        <div>
+          <form>
+            <FormInput
+              label="DisplayName"
+              type="text"
+              autoComplete="off"
+              required
+              name="displayName"
+              value=""
+            />
+            <FormInput
+              label="Email"
+              type="email"
+              autoComplete="off"
+              required
+              name="email"
+              value=""
+            />
 
-        <div className="bgImg"></div>
-        <div className="content">
-          <div className="d-flex h-100 align-items-center justify-content-center">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-4 col-sm-6 ml-auto mr-auto">
-                  <div className="card">
-                    <div className="card-body">
-                      <form>
-                        <div className="glowBox">
-                          <h4 className="title">Sign Up</h4>
-                          <div className="socialMedia">
-                            <a href="#" className="btn btn-just-icon btn-link">
-                              <i className="fab fa-facebook-square"></i>
-                            </a>
-                            <a href="#" className="btn btn-just-icon btn-link">
-                              <i className="fab fa-twitter"></i>
-                            </a>
-                            <a href="#" className="btn btn-just-icon btn-link">
-                              <i className="fab fa-google-plus"></i>
-                            </a>
-                          </div>
-                        </div>
-                      </form>
-                      <div className="desc text-center">
-                        <p>Sign up now to use our services!</p>
-                      </div>
-                      <div className="signUp">
-                        <span>
-                          <input
-                            type="text"
-                            className="form"
-                            placeholder="Name"
-                            autocomplete="off"
-                            style="cursor: auto"
-                          />
-                          <span className="underline"></span>
-                        </span>
-                        <span>
-                          <input
-                            type="text"
-                            className="form"
-                            placeholder="Email"
-                            autocomplete="off"
-                            style="cursor: auto"
-                          />
-                          <span className="underline"></span>
-                        </span>
-                        <span>
-                          <input
-                            type="password"
-                            className="form"
-                            placeholder="Password"
-                            autocomplete="off"
-                            style="cursor: auto"
-                          />
-                          <span className="underline"></span>
-                        </span>
-                        <span>
-                          <input
-                            type="password"
-                            className="form"
-                            placeholder="Confirm Password"
-                            autocomplete="off"
-                            style="cursor: auto"
-                          />
-                          <span className="underline"></span>
-                        </span>
-                      </div>
-                      <div className="text-center">
-                        <a
-                          href="#"
-                          className="btn btn-primary btn-link btn-wd btn-lg"
-                        >
-                          Sign Up
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+            <FormInput
+              label="Password"
+              type="password"
+              autoComplete="off"
+              required
+              name="password"
+              value=""
+            />
+
+            <FormInput
+              label="Confirm Password"
+              type="password"
+              autoComplete="off"
+              required
+              name="confirmPassword"
+              value=""
+            />
+            <button type="submit">Sign Up</button>
+          </form>
         </div>
-      
+      </div>
+      </div>
     </>
   );
 };
