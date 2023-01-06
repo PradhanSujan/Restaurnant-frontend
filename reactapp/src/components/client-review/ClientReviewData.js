fae308fe0c37e8754a5620575da23d7384793133
Bishal Shrestha <bbrobishal@gmail.com>
2023-01-06 21:09:43 +0545

    showing backend validating message through toast just only email and password other remeaning

--- reactapp/src/components/client-review/ClientReviewData.js
+++ reactapp/src/components/client-review/ClientReviewData.js
@@ -1,16 +1,17 @@
-// import client from '../../assets/images/client.png'
 const ClientReviewData = [
     {
-        name: "Hari Karki",
+        name: "Bishal Shrestha",
         address:"Kapan, Kathmandu",
+        image:"images/bishal-shrestha.png",
         review:`For a perfect date, a business meeting, lone time or casual
                   family dine, this place is one of the best fit in Kathmandu.
                   Great food, enough space with great ambience and enough
                   parking space, plus great hospitality. Highly recommended.`
     },
     {
-        name: "Sita Rai",
-        address:"Chabahil, Kathamandu",
+        name: "Sujan Pradhan",
+        address:"Tinkune, Kathamandu",
+        image:"images/sujan-pradhan.png",
         review:`For a perfect date, a business meeting, lone time or casual
                   family dine, this place is one of the best fit in Kathmandu.
                   Great food, enough space with great ambience and enough
